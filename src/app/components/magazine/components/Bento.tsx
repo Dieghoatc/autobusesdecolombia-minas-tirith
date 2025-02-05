b1e7eadc4362264c22df475ec81d95df40702caf
dieghoatc <dieghoatc@gmail.com>
2025-02-04 23:16:57 -0500

    fix: skeleton magazine

--- src/app/components/magazine/components/Bento.tsx
+++ src/app/components/magazine/components/Bento.tsx
@@ -4,10 +4,11 @@ import { BentoItem } from "./BentoItem";
 import { ApiPostsResponse } from "@/app/api/autobusesApi.interfaces";
 import { useHookFetch } from "@/app/hooks/useHookFetch";
 import { orderById } from "@/app/utils/orderById";
+import { SkeletonBento } from "../../skeleton/SkeletonBento";
 import "./bento.css";
 
 export function Bento() {
-  const { data } = useHookFetch<ApiPostsResponse[]>("posts");
+  const { data, loading } = useHookFetch<ApiPostsResponse[]>("posts");
 
   function orderHighestToLowestPosts(data: ApiPostsResponse[]) {
     return orderById(data, "post_id");
@@ -16,17 +17,25 @@ export function Bento() {
   const sortedData = orderHighestToLowestPosts(data as ApiPostsResponse[]);
 
   return (
-    <section className="bento">
-      {sortedData.map((post: ApiPostsResponse) => {
-        const { post_id, slug } = post;
-        return (
-          <div key={post_id} className="bento-primary">
-            <Link href={`/pages/posts/${post_id}_${slug}`}>
-              <BentoItem post={post} />
-            </Link>
-          </div>
-        );
-      })}
-    </section>
+    <div>
+      {loading ? (
+        sortedData.map((post: ApiPostsResponse) => {
+          const { post_id, slug } = post;
+          return (
+            <section key={post_id} className="bento">
+              <div className="bento-primary">
+                <Link href={`/pages/posts/${post_id}_${slug}`}>
+                  <BentoItem post={post} />
+                </Link>
+              </div>
+            </section>
+          );
+        })
+      ) : (
+        <section className="bento">
+          <SkeletonBento skeletonStyle={"bento-primary"} />
+        </section>
+      )}
+    </div>
   );
 }
