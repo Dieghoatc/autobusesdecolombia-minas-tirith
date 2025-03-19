e12860d2059f831145bde2acce1ff898b8ad33a4
dieghoatc <dieghoatc@gmail.com>
2025-03-19 01:22:56 -0500

    fix

--- src/app/utils/getFirstItemToArray.ts
+++ src/app/utils/getFirstItemToArray.ts
@@ -1,7 +1,7 @@
 import { ApiPhotosResponse } from "../api/dto/photo.dto";
 import { orderById } from "./orderById";
 
-export function getFirstItemToArray(data: ApiPhotosResponse){
+export function getFirstItemToArray(data: ApiPhotosResponse[]){
   const orderArray = orderById(data, "photo_id");
 
   return orderArray[0];
