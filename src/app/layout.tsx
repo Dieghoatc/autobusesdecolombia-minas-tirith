0ab5c885b6ef233148f61ce43436af2565f7e5d9
dieghoatc <dieghoatc@gmail.com>
2025-03-21 00:28:57 -0500

    feat: update keyworks seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   description:
     "Descubre la mayor comunidad de aficionados a los Autobuses de Colombia. Portal de fotografia, noticias, historias y eventos sobre el apasionante mundo del transporte público. Únete a nuestra comunidad y comparte tu pasión por los autobuses y el transporte en Colombia.",
   keywords:
-    "autobuses de colombia, autobuses en colombia, autobuses, buses, interdepartamental, intermunicipal, urbanos, fotografía, transporte, comunidad, historias, noticias, galerias, información, buses de colombia",
+    "autobuses de colombia, autobuses en colombia, buses de colombia, autobus colombia, bus colombia, autobuses colombia, buses colombianos, bus de colombia, autobuses colombianos, autobuses, buses, buses en colombia, buses de colombia, bus en colombia, colombian bus",
 }
 
 export default function RootLayout({
