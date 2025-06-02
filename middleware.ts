794ed43b01c1fdaa5f0a4e777f8c57f551302c2f
dieghoatc <dieghoatc@gmail.com>
2025-06-02 18:00:36 -0500

    fix: test image logo

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   console.log("✅ Middleware ejecutado en:", request.nextUrl.pathname);
   const token = request.cookies.get("access_token")?.value;
   console.log("🚀 Token:", token);
 
-  // if (
-  //   request.method === "GET" &&
-  //   !token &&
-  //   request.nextUrl.pathname.startsWith("/upload")
-  // ) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
+  if (
+    request.method === "GET" &&
+    !token &&
+    request.nextUrl.pathname.startsWith("/upload")
+  ) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   return NextResponse.next();
 }
@@ -21,4 +21,4 @@ export const config = {
   matcher: ["/upload", "/upload/:path*"],
 };
 
-export const runtime = 'nodejs';  
+export const runtime = "nodejs";
