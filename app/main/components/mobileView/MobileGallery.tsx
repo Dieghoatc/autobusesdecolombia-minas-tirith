601148803e6c6ae6b8e0c7b29a0bb82d987eabed
dieghoatc <dieghoatc@gmail.com>
2025-06-03 22:40:34 -0500

    fix: import add horizontal

--- app/main/components/mobileView/MobileGallery.tsx
+++ app/main/components/mobileView/MobileGallery.tsx
@@ -1,7 +1,7 @@
 import { LastPhotos } from "../gallery/components/lastPhotos";
 import { CategoryList } from "../gallery/components/categoryList";
 import { ApiPhotosResponse } from "@/services/types/photo.type";
-import AdHorizontal from "@/components/adsense/AdHorizontal";
+import { AdHorizontal } from "@/components/adsense/AdHorizontal";
 
 interface MobileGalleryProps {
   images: ApiPhotosResponse[];
