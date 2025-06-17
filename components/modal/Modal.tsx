fea78f0b363a3680aa693296853302627ad800ac
dieghoatc <dieghoatc@gmail.com>
2025-06-17 04:55:00 -0500

    feat: uptade styles modal, and add bottons controls

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-
+import { X } from "lucide-react";
 import styles from "./Modal.module.css";
 
 interface ModalProps {
@@ -11,17 +11,33 @@ interface ModalProps {
 }
 
 export function Modal({ children, onClose, isOpen }: ModalProps) {
+  
   useEffect(() => {
     if (isOpen) {
+      window.history.pushState({ modal: true }, "");
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "auto";
     }
 
+    const handlePopState = (e: PopStateEvent) => {
+      if (isOpen) {
+        onClose();
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
     return () => {
       document.body.style.overflow = "auto";
+      window.removeEventListener("popstate", handlePopState);
+      // Opcional: limpiar el historial extra si es necesario
+      if (window.history.state?.modal) {
+        window.history.back();
+      }
     };
-  }, [isOpen]);
+  }, [isOpen, onClose]);
 
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
@@ -33,13 +49,11 @@ export function Modal({ children, onClose, isOpen }: ModalProps) {
 
   return (
     <div className={styles.overlay} onClick={handleOverlayClick}>
+      {children}
       <div className={styles.button}>
         <button className={styles.close_button} onClick={onClose}>
-          x
+          <X size={30} />
         </button>
-      </div>
-      <div className={styles.modal_container}>
-        <div>{children}</div>
       </div>
     </div>
   );
