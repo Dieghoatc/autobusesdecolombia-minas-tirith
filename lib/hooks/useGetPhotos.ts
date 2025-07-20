0ba8f93e3c7a78f7fc0d6763e4758f76193da392
dieghoatc <dieghoatc@gmail.com>
2025-07-20 05:29:03 -0500

    fix: delete call store because no is using

--- lib/hooks/useGetPhotos.ts
+++ lib/hooks/useGetPhotos.ts
@@ -3,15 +3,12 @@
 import { useState, useEffect } from "react";
 import { photosQuery } from "@/services/api/photos.query";
 import { ApiPhotoResponse, Photo } from "@/services/types/photo.type";
-import usePhotosStore from "@/lib/store/usePhotosStore";
 
 export function useGetPhotos(page: number, limit: number) {
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-  const setPhotosStore = usePhotosStore((state) => state.setPhotos);
-  const setLoadingStore = usePhotosStore((state) => state.setLoading);
 
   useEffect(() => {
     async function fetchPhotos(page: number, limit: number) {
@@ -22,16 +19,14 @@ export function useGetPhotos(page: number, limit: number) {
         const result = await photosQuery(page, limit); 
         const { data } = result || {} as ApiPhotoResponse;    
         setPhotos(data);
-        setPhotosStore(data);
       } catch (error) {
         setError(`Error to fetch data: ${error}`);
       } finally {
-        setLoadingStore(false);
         setLoading(false);
       }
     }
     fetchPhotos(page, limit);
-  }, [setPhotosStore, setLoadingStore, page]);
+  }, [page]);
 
   return { photos, loading, error };
 }
