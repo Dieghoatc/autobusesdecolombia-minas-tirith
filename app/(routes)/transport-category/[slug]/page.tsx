fc3304829579297dd0c881c8d2946779e0c28696
dieghoatc <dieghoatc@gmail.com>
2025-07-25 08:22:12 -0500

    feat: Add styles title and description in categories page

--- app/(routes)/transport-category/[slug]/page.tsx
+++ app/(routes)/transport-category/[slug]/page.tsx
@@ -9,14 +9,16 @@ import { PaginationGallery } from "@/components/paginationGallery/paginationGall
 
 import styles from "./CategoryGallery.module.css";
 import { SkeletonGallery } from "../components/SkeletonGallery";
-import { useTransportCategoryStore } from "@/lib/store/useTransportCategoryStore";
+import { useTransportCategories } from "@/lib/hooks";
 
 export default function CategoryGallery() {
   const searchParams = useSearchParams();
   const { slug } = useParams<{ slug: string }>();
   const router = useRouter();
   const { transportCategories, loading: loadingCategories } =
-    useTransportCategoryStore();
+    useTransportCategories();
+
+  console.log(transportCategories);
 
   const page = parseInt(searchParams.get("page") || "1");
   const limit = parseInt(searchParams.get("limit") || "20");
@@ -44,15 +46,15 @@ export default function CategoryGallery() {
   return (
     <div>
       <section className={styles.container}>
-          <div className="w-full bg-black flex flex-col md:flex-row items-center justify-center px-4 py-20">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-6 leading-tight">  
-            <span className="text-white">{categoryTitle[0] + " "}</span>            
-            <span className="text-amber-500">{categoryTitle[1]}</span>
-            </h1>
-            <p className="text-lg md:text-xl text-gray-300 mb-8 max-w-2xl mx-auto leading-relaxed">
-              {categoryDescription}
-            </p>
-          </div>
+        <div className={styles.header}>
+          <h1 className={styles.title}>
+            {categoryTitle[0] + " "}
+            <span>{categoryTitle[1]}</span>
+          </h1>
+          <p className={styles.description}>
+            {categoryDescription}
+          </p>
+        </div>
         <div className={styles.list}>
           {vehicles.data.map((vehicle) => (
             <GalleryList key={vehicle.vehicle_id} vehicle={vehicle} />
