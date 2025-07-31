228c1db85600e6fb9fdb4a12425689b686274b4b
dieghoatc <dieghoatc@gmail.com>
2025-07-31 00:16:53 -0500

    feat: add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Toaster />
-        <Analytics />       
+        <Analytics />
       </body>
     </html>
   );
