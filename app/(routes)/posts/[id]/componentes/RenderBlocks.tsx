7a30e9a0e8c54b552f7b0cd5fc016c21c9c89ef4
dieghoatc <dieghoatc@gmail.com>
2025-07-30 21:54:16 -0500

    fix: typo

--- app/(routes)/posts/[id]/componentes/RenderBlocks.tsx
+++ app/(routes)/posts/[id]/componentes/RenderBlocks.tsx
@@ -1,4 +1,3 @@
-import { PostBlock } from "../post_example";
 import { Header } from "./header/Header";
 import { Title } from "./title/Title";
 import { Author } from "./author/Author";
@@ -9,6 +8,7 @@ import { List } from "./list/List";
 import { Fact } from "./fact/Fact";
 
 import styles from "./RenderBlocks.module.css";
+import { PostBlock } from "@/services/types/post.type";
 
 interface RenderBlocksProps {
   blocks: PostBlock[];
@@ -51,20 +51,20 @@ export function RenderBlocks({ blocks }: RenderBlocksProps) {
               </section>
             );
           case "paragraph":
-            return <Paragraph key={i} level={block.level} text={block.text} />
+            return <Paragraph key={i} level={block.level} text={block.text} />;
 
           case "quote":
-            return <Quote key={i} text={block.text} author={block.author} />
-            
+            return <Quote key={i} text={block.text} author={block.author} />;
+
           case "list":
-            return <List key={i} style={block.style} items={block.items} />
+            return <List key={i} style={block.style} items={block.items} />;
 
           case "fact":
             return (
               <section key={i}>
                 <Fact title={block.heading} text={block.text} />
               </section>
-            )
+            );
 
           default:
             return null;
