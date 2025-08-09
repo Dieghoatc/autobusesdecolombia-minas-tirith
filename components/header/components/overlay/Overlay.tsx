f8465936cc38840728abf4707a19259677f16930
Dieghoatc <dieghoatc@gmail.com>
2025-08-08 19:52:57 -0500

    refactor: head component

--- components/header/components/overlay/Overlay.tsx
+++ components/header/components/overlay/Overlay.tsx
@@ -2,8 +2,7 @@ import styles from "./Overlay.module.css";
 import { Item } from "../Item";
 import Link from "next/link";
 
-import { Skeleton } from "@/components/ui/skeleton";
-import { useTransportCategoryStore } from "@/lib/store/useTransportCategoryStore";
+import { menuCategories } from "./menuCategories";
 
 interface OverlayProps {
   open: boolean;
@@ -11,8 +10,6 @@ interface OverlayProps {
 }
 
 export function Overlay({ open, setOpen }: OverlayProps) {
-  const { transportCategories, loading } = useTransportCategoryStore();
-
   return (
     <section className={`${styles.container} ${open ? "" : styles.close}`}>
       <div className={styles.content}>
@@ -20,20 +17,9 @@ export function Overlay({ open, setOpen }: OverlayProps) {
         <div className={styles.content_categories}>
           <h4>Categorias</h4>
           <ul className={styles.content_categories_list}>
-            {loading ? (
-              <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]" />
-                <Skeleton className="h-4 w-[200px]" />
-              </div>
-            ) : (
-              transportCategories.map((category) => (
-                <Item
-                  key={category.transport_category_id}
-                  category={category}
-                  setOpen={setOpen}
-                />
-              ))
-            )}
+            {menuCategories.map((category) => (
+              <Item key={category.id} category={category} setOpen={setOpen} />
+            ))}
           </ul>
         </div>
       </div>
---
+++ components/header/components/overlay/menuCategories.ts
@@ -0,0 +1,43 @@
+export interface MenuCategory {
+  id: number;
+  name: string;
+}
+
+export const menuCategories: MenuCategory[] = [
+  {
+    id: 1,
+    name: "Transporte Intermunicipal",
+  },
+  {
+    id: 2,
+    name: "Transporte Intermunicipal",
+  },
+  {
+    id: 3,
+    name: "Nuestros Recuerdos",
+  },
+  {
+    id: 4,
+    name: "Turismo",
+  },
+  {
+    id: 5,
+    name: "Transporte Especial",
+  },
+  {
+    id: 6,
+    name: "Transporte Escolar",
+  },
+  {
+    id: 7,
+    name: "Chivas",
+  },
+  {
+    id: 8,
+    name: "Transporte Urbano",
+  },
+  {
+    id: 9,
+    name: "Taxi",
+  },
+];
