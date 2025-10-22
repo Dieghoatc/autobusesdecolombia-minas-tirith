530025c68ee91b2f64b94cdc0b3dae6deac4f649
Dieghoatc <dieghoatc@gmail.com>
2025-10-22 05:22:55 -0500

    fix: view aside, sidebar version mobile

--- app/main/Main.tsx
+++ app/main/Main.tsx
@@ -5,21 +5,43 @@ import { Magazine } from "./components/magazine";
 import { Sidebar } from "./components/sidebar";
 
 import { useShowSidebarMenu } from "@/lib/store/useShowSidebarMenu";
+import { useIsMobile } from "@/lib/hooks/useIsMobile";
 
 export default function Main() {
   const { open } = useShowSidebarMenu();
+  const isMobile = useIsMobile();
 
   return (
-    <div className="grid grid-cols-12 overflow-hidden">
-      {open && (
-        <div className="sticky top-0 col-span-2 h-[100vh] overflow-y-auto">
+    <div className="grid grid-cols-12 relative h-screen overflow-hidden">
+      {!isMobile && open && (
+        <aside className="col-span-2 sticky top-0 h-screen overflow-y-auto z-10 bg-black">
           <Sidebar />
+        </aside>
+      )}
+      {isMobile && open && (
+        <div className="absolute inset-0 z-50 bg-black/40">
+          <aside
+            className="absolute top-0 left-0 w-4/5 h-full bg-black shadow-lg overflow-y-auto"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <Sidebar />
+          </aside>
         </div>
       )}
-      <div className={`${open ? "col-span-10" : "col-span-12"} h-[100vh] overflow-y-auto`}>
+
+      <main
+        className={`
+          ${
+            !isMobile && open
+              ? "col-span-10"
+              : "col-span-12"
+          } 
+          h-screen overflow-y-auto transition-all duration-300
+        `}
+      >
         <Magazine />
         <Gallery />
-      </div>
+      </main>
     </div>
   );
 }
