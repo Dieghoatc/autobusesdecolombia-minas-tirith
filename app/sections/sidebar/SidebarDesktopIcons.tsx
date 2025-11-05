b9e1157c840d3fbb35903a8ce2f1095b56696710
Dieghoatc <dieghoatc@gmail.com>
2025-11-05 05:30:30 -0500

    fix: styles sidebar menu

--- app/sections/sidebar/SidebarDesktopIcons.tsx
+++ app/sections/sidebar/SidebarDesktopIcons.tsx
@@ -9,28 +9,28 @@ export function SidebarDesktopIcons() {
     return (
         <aside className="w-14 h-screen px-2 pt-4 flex flex-col items-center">
             <div className="fixed h-screen">
-                <div className='flex flex-col gap-5'>
+                <div className='flex flex-col gap-2'>
                     <ul className='h-16 w-full flex items-center justify-center'>
                         <li> <ArrowRightToLine onClick={() => setOpenDesktop(true)} /></li>
                     </ul>
-                    <ul className={`${styles.list_item} flex flex-col gap-2`}>
+                    <ul className={`${styles.logo} flex flex-col gap-2 border-b border-gray-700 py-4`}>
                         <li> <House /></li>
                         <li> <Newspaper /></li>
                         <li> <Bus /></li>
                         <li> <Factory /></li>
                     </ul>
-                    <ul className={`${styles.list_item} flex flex-col gap-2`}>
+                    <ul className={`${styles.logo} flex flex-col gap-2 border-b border-gray-700 py-4`}>
                         <li> <Route /></li>
                         <li> <Layers /></li>
                         <li> <MapPinCheck /></li>
                     </ul>
-                    <ul className={`${styles.list_item} flex flex-col gap-2`}>
+                    <ul className={`${styles.logo} flex flex-col gap-2 border-b border-gray-700 py-4`}>
                         <li> <Images /></li>
                         <li> <User /></li>
                         <li> <Users /></li>
                         <li> <Rss /></li>
                     </ul>
-                    <ul className={`${styles.list_item} flex flex-col gap-2`}>
+                    <ul className={`${styles.logo} flex flex-col gap-2 border-b border-gray-700 py-4`}>
                         <li> <Mail /></li>
                         <li> <File /></li>
                         <li> <FileMinus /></li>
--- app/sections/sidebar/components/MenuList.tsx
+++ app/sections/sidebar/components/MenuList.tsx
@@ -6,30 +6,30 @@ export function MenuList() {
     return (
         <>
             <section>
-                <ul className="border-b border-gray-700 flex flex-col gap-2 py-5 text-white text-sm font-semibold ">
-                    <li><Link href="/"><div className={styles.list_item}><House /><span>Inicio</span></div></Link></li>
-                    <li><Link href="/noticias"><div className={styles.list_item}><Newspaper /><span>Noticias</span></div></Link></li>
-                    <li><Link href="/empresas-de-transporte"><div className={styles.list_item}><Bus /><span>Empresas de Transporte</span></div></Link></li>
-                    <li><Link href="/empresas-fabricantes"><div className={styles.list_item}><Factory /><span>Empresas Fabricantes</span></div></Link></li>
+                <ul className="border-b border-gray-700 flex flex-col gap-2 py-4 text-white text-sm font-semibold ">
+                    <li><Link href="/"><div className={`${styles.logo} flex gap-2`}><House /><span>Inicio</span></div></Link></li>
+                    <li><Link href="/noticias"><div className={`${styles.logo} flex gap-2`}><Newspaper /><span>Noticias</span></div></Link></li>
+                    <li><Link href="/empresas-de-transporte"><div className={`${styles.logo} flex gap-2`}><Bus /><span>Empresas de Transporte</span></div></Link></li>
+                    <li><Link href="/empresas-fabricantes"><div className={`${styles.logo} flex gap-2`}><Factory /><span>Empresas Fabricantes</span></div></Link></li>
                 </ul>
                 <h2 className="mt-2 font-bold">Transporte</h2>
                 <ul className="border-b border-gray-700 flex flex-col gap-2 py-4 text-white text-sm font-semibold">
-                    <li><Link href="/rutas-de-transporte"><div className={styles.list_item}><Route /><span>Rutas</span></div></Link></li>
-                    <li><Link href="/terminales-de-transporte"><div className={styles.list_item}><Layers /><span>Terminales de Transporte</span></div></Link></li>
-                    <li><Link href="/destinos"><div className={styles.list_item}><MapPinCheck /><span>Destinos</span></div></Link></li>
+                    <li><Link href="/rutas-de-transporte"><div className={`${styles.logo} flex gap-2`}><Route /><span>Rutas</span></div></Link></li>
+                    <li><Link href="/terminales-de-transporte"><div className={`${styles.logo} flex gap-2`}><Layers /><span>Terminales de Transporte</span></div></Link></li>
+                    <li><Link href="/destinos"><div className={`${styles.logo} flex gap-2`}><MapPinCheck /><span>Destinos</span></div></Link></li>
                 </ul>
                 <h2 className="mt-2 font-bold">Nosotros</h2>
                 <ul className="border-b border-gray-700 flex flex-col gap-2 py-4 text-white text-sm font-semibold">
-                    <li><Link href="/galerias"><div className={styles.list_item}><Images /><span>Galerias</span></div></Link></li>
-                    <li><Link href="/quienes-somos"><div className={styles.list_item}><User /><span>Quienes Somos</span></div></Link></li>
-                    <li><Link href="/comunidad"><div className={styles.list_item}><Users /><span>Comunidad</span></div></Link></li>
-                    <li><Link href="/blog"><div className={styles.list_item}><Rss /><span>Blog</span></div></Link></li>
+                    <li><Link href="/galerias"><div className={`${styles.logo} flex gap-2`}><Images /><span>Galerias</span></div></Link></li>
+                    <li><Link href="/quienes-somos"><div className={`${styles.logo} flex gap-2`}><User /><span>Quienes Somos</span></div></Link></li>
+                    <li><Link href="/comunidad"><div className={`${styles.logo} flex gap-2`}><Users /><span>Comunidad</span></div></Link></li>
+                    <li><Link href="/blog"><div className={`${styles.logo} flex gap-2`}><Rss /><span>Blog</span></div></Link></li>
                 </ul>
                 <h2 className="mt-2 font-bold">Legal</h2>
                 <ul className="border-b border-gray-700 flex flex-col gap-2 py-4 text-white text-sm font-semibold">
-                    <li><Link href="/contacto"><div className={styles.list_item}><Mail /><span>Contacto</span></div></Link></li>
-                    <li><Link href="/politica-de-privacidad"><div className={styles.list_item}><File /><span>Política de Privacidad</span></div></Link></li>
-                    <li><Link href="/terminos-condiciones"><div className={styles.list_item}><FileMinus /><span>Terminos y Condiciones</span></div></Link></li>
+                    <li><Link href="/contacto"><div className={`${styles.logo} flex gap-2`}><Mail /><span>Contacto</span></div></Link></li>
+                    <li><Link href="/politica-de-privacidad"><div className={`${styles.logo} flex gap-2`}><File /><span>Política de Privacidad</span></div></Link></li>
+                    <li><Link href="/terminos-condiciones"><div className={`${styles.logo} flex gap-2`}><FileMinus /><span>Terminos y Condiciones</span></div></Link></li>
                 </ul>
             </section>
             <footer className="mt-20 font-semibold text-gray-400 text-xs text-center">
